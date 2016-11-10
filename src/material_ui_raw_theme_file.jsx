e22b89f5a9652ab4291cacd3ce34cfcf419599b5
Luís Ramalho <info@luisramalho.com>
2016-11-10 15:18:22 +0200

    Updated brand colors

--- src/material_ui_raw_theme_file.jsx
+++ src/material_ui_raw_theme_file.jsx
@@ -6,14 +6,14 @@ module.exports = {
   spacing: Spacing,
   fontFamily: 'Roboto, sans-serif',
   palette: {
-    primary1Color: Colors.lightGreen500,
-    primary2Color: Colors.lightGreen700,
+    primary1Color: Colors.yellow500,
+    primary2Color: Colors.yellow700,
     primary3Color: Colors.lightBlack,
     accent1Color: Colors.pinkA200,
     accent2Color: Colors.grey100,
     accent3Color: Colors.grey500,
     textColor: Colors.darkBlack,
-    alternateTextColor: Colors.white,
+    alternateTextColor: Colors.black,
     canvasColor: Colors.white,
     borderColor: Colors.grey300,
     disabledColor: ColorManipulator.fade(Colors.darkBlack, 0.3)
