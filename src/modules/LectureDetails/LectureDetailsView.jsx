a2a57252c742a4ba2b46b8f9c0153954fbc37474
Luís Ramalho <info@luisramalho.com>
2016-11-17 12:04:43 +0200

    Implemented the detail view for lecture detail

--- src/modules/LectureDetails/LectureDetailsView.jsx
+++ src/modules/LectureDetails/LectureDetailsView.jsx
@@ -1,7 +1,31 @@
 import { Component, PropTypes } from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import Avatar from 'material-ui/Avatar';
+import Chip from 'material-ui/Chip';
+import Moment from 'moment';
+import _ from 'lodash';
 import {
   get
 } from '../../utils/api';
+
+const styles = {
+  chip: {
+    margin: 4
+  },
+  wrapper: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  }
+};
+
+function handleRequestDelete() {
+  alert('You clicked the delete button.');
+}
+
+function handleTouchTap() {
+  alert('You clicked the Chip.');
+}
 
 class LectureDetail extends Component {
   constructor(props) {
@@ -19,9 +43,46 @@ class LectureDetail extends Component {
 
   render() {
     return(
-      <div>
-        { JSON.stringify(this.state.lecture) }
-      </div>
+      <Card style={{
+          margin: this.context.muiTheme.spacing.desktopGutter / 2,
+          flex: 1,
+          flexBasis: '450px'
+        }}>
+        <CardHeader
+          title={'User #ID' + this.state.lecture.creatorId}
+          subtitle={Moment(this.state.lecture.dates).format("D MMMM YYYY, h:mm")}
+          avatar="http://lorempixel.com/128/128/people"
+        >
+        <Chip onTouchTap={handleTouchTap} style={styles.chip}>
+          <Avatar size={32}>{String(this.state.lecture.creatorType).charAt(0)}</Avatar>
+          {_.capitalize(this.state.lecture.creatorType)}
+        </Chip>
+      </CardHeader>
+        <CardTitle
+          title={this.state.lecture.title}
+          subtitle={Moment(this.state.lecture.dates).format("D MMMM YYYY, h:mm")} />
+        <CardText>
+          <h3>Description</h3>
+          <p>{this.state.lecture.description}</p>
+          <h3>Notes</h3>
+          <h4>Teacher Notes</h4>
+          <p>{this.state.lecture.teacherNote}</p>
+          <h4>Expert Notes</h4>
+          <p>{this.state.lecture.expertNote}</p>
+          <h3>Target Students</h3>
+          <p>{this.state.lecture.targetStudents}</p>
+          <h3>Area</h3>
+          <p>{this.state.lecture.area}</p>
+          <hr/>
+          <p>
+            { JSON.stringify(this.state.lecture) }
+          </p>
+        </CardText>
+        <CardActions>
+          <FlatButton label="Action1" />
+          <FlatButton label="Action2" />
+        </CardActions>
+      </Card>
     );
   }
 }
